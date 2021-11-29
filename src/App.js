4ff96e7f732d65f3d15d601c3492a8002ee0b4fe
vidisharaut <vidisharaut27@gmail.com>
2021-11-29 20:04:26 +0530

    Added css in index.css file

--- src/App.js
+++ src/App.js
@@ -6,7 +6,15 @@ const api = {
 };
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search..." />
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
