64df95dde6d30675d1d1f23501e18cd17367f739
vidisharaut <vidisharaut27@gmail.com>
2021-11-29 18:50:05 +0530

    Added background images to the assets folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
+const api = {
+  key: "04ba21e02c8750e703c5b352bde4e3b9",
+  base: "https://api.openweather.org/data/2.5/",
+};
+
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">Hello</div>;
 }
 
 export default App;
